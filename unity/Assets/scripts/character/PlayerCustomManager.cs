ecc21996af557a6bd35017c7ca6c29a72a3cb6b3
lonnstyle <54892447+lonnstyle@users.noreply.github.com>
2022-02-13 15:16:02 +0800

    multi-player connection (#25)
    * feat: multiplayer setup
    match making and lobby scripts done, check script comments for TODO issues
    
    * fix: canvas orders and player photon view
    
    * fix: check IsMine and player prefab update
    
    * feat: sync player color and facing
    
    * feat: sync player color
    
    * fix: remove unused logs
    
    * fix: player collision cause y-coordinate increase

--- unity/Assets/scripts/character/PlayerCustomManager.cs
+++ unity/Assets/scripts/character/PlayerCustomManager.cs
@@ -1,20 +1,30 @@
+using Photon.Pun;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerCustomManager : MonoBehaviour
+public class PlayerCustomManager : MonoBehaviourPun
 {
     [SerializeField] SpriteRenderer playerRenderer;
     private float[] colors = { 0f, 0f, 0f };
 
     void Start()
-    {
+    {   
+        if (photonView.IsMine == false && PhotonNetwork.IsConnected == true)
+        {
+            return;
+        }
         colors[0] = PlayerPrefs.GetFloat("characterColorR");
         colors[1] = PlayerPrefs.GetFloat("characterColorG");
         colors[2] = PlayerPrefs.GetFloat("characterColorB");
         Color newColor = new Color(colors[0], colors[1], colors[2]);
-        playerRenderer.color = newColor;
         Debug.Log(newColor.ToString());
-        Debug.Log(colors.ToString());
+        photonView.RPC("RPC_changeColor",RpcTarget.AllBuffered,colors[0], colors[1], colors[2]);
+    }
+    [PunRPC]
+    void RPC_changeColor(float R,float G,float B)
+    {
+        Color newColor = new Color(R,G,B);
+        playerRenderer.color = newColor;
     }
 }
