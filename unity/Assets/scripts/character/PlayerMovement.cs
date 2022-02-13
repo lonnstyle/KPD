d1e4862b6fba2c2cb90206131832dd10fa407dc0
lonnstyle <54892447+lonnstyle@users.noreply.github.com>
2022-02-13 15:16:02 +0800

    multi-player connection (#25)
    * feat: multiplayer setup
    match making and lobby scripts done, check script comments for TODO issues
    
    * fix: canvas orders and player photon view
    
    * fix: check IsMine and player prefab update
    
    * feat: sync player color and facing
    
    * feat: sync player color
    
    * fix: remove unused logs
    
    * fix: player collision cause y-coordinate increase

--- unity/Assets/scripts/character/PlayerMovement.cs
+++ unity/Assets/scripts/character/PlayerMovement.cs
@@ -1,8 +1,9 @@
+using Photon.Pun;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+public class PlayerMovement : MonoBehaviourPun
 {
 
     public CharacterController2D controller;
@@ -13,12 +14,16 @@ public class PlayerMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (photonView.IsMine == false && PhotonNetwork.IsConnected == true)
+        {
+            return;
+        }//check if it is self character
         horizontalMove = Input.GetAxisRaw("Horizontal");
         verticalMove = Input.GetAxisRaw("Vertical");
     }
@@ -26,6 +31,6 @@ public class PlayerMovement : MonoBehaviour
     void FixedUpdate()
     {
         // Move the player character
-        controller.Move(horizontalMove,verticalMove);
+        controller.Move(horizontalMove, verticalMove);
     }
 }
