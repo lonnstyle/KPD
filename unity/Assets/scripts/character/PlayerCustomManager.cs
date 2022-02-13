60f925dbea0663a28f0bcfa25a9bc5e978a84c8f
lonnstyle <lonnstyle@gmail.com>
2022-02-13 12:55:29 +0800

    feat: sync player color and facing

--- unity/Assets/scripts/character/PlayerCustomManager.cs
+++ unity/Assets/scripts/character/PlayerCustomManager.cs
@@ -1,14 +1,19 @@
+using Photon.Pun;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerCustomManager : MonoBehaviour
+public class PlayerCustomManager : MonoBehaviourPun
 {
     [SerializeField] SpriteRenderer playerRenderer;
     private float[] colors = { 0f, 0f, 0f };
 
     void Start()
-    {
+    {   
+        if (photonView.IsMine == false && PhotonNetwork.IsConnected == true)
+        {
+            return;
+        }
         colors[0] = PlayerPrefs.GetFloat("characterColorR");
         colors[1] = PlayerPrefs.GetFloat("characterColorG");
         colors[2] = PlayerPrefs.GetFloat("characterColorB");
