e10708b081c35393aacdeb30dd3a8b52bc6e55c4
lonnstyle <lonnstyle@gmail.com>
2022-02-13 23:15:12 +0800

    loading task and LeanTween library
    the loading task contains a loading tween bar and a lean tween library

---
+++ unity/Assets/scripts/minigames/loading task/loadingTask.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DentedPixel;
+using TMPro;
+
+public class loadingTask : MonoBehaviour
+{
+    public GameObject bar;
+    public int LoadingTime;
+    public TMP_Text timerText;
+    public float timer = 0.0f;
+    public bool istimer = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+    // Update is called once per frame
+    void Update()
+    {   if (istimer)
+        {  
+            timer += Time.deltaTime;
+            DisplayTime();
+        }
+        if (Mathf.Floor(timer) == LoadingTime)
+        {
+            istimer = false;
+            main.Instance.SwitchChange(1);
+        }
+    }
+    public void AnimateBar()
+    {   
+        istimer = true;
+        LeanTween.scaleX(bar,14,LoadingTime);
+        //the length the bar scale up right near to the background is 14 
+    }
+    void DisplayTime()
+    {
+        int minutes = Mathf.FloorToInt(timer/60.0f);
+        int seconds = Mathf.FloorToInt(timer - minutes *60);
+        timerText.text = string.Format("Loading time: {0:0}:{1:00}", minutes,seconds);
+    }
+     
+}
