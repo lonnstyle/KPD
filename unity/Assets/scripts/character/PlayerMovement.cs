5441fb63568dc5d01c08d2fb8a498f454ae6fe7c
lonnstyle <lonnstyle@gmail.com>
2022-04-02 17:55:50 +0800

    feat: kill players

--- unity/Assets/scripts/character/PlayerMovement.cs
+++ unity/Assets/scripts/character/PlayerMovement.cs
@@ -12,6 +12,8 @@ public class PlayerMovement : MonoBehaviourPun
     private Rigidbody2D m_Rigidbody2D;
 
     private bool collidePlayer = false;
+    public GameObject otherPlayer;
+    private Animator animator;
 
     // Start is called before the first frame update
     void Start()
@@ -22,6 +24,7 @@ public class PlayerMovement : MonoBehaviourPun
     private void Awake()
     {
         m_Rigidbody2D = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -35,26 +38,37 @@ public class PlayerMovement : MonoBehaviourPun
         verticalMove = Input.GetAxisRaw("Vertical");
         if (collidePlayer && Input.GetKeyDown(KeyCode.F))
         {
-            Debug.Log("I'm trying to kill someone");
+            Debug.Log("I'm trying to kill someone" + otherPlayer);
+            // otherPlayer.SendMessage("killed");
+            otherPlayer.GetComponent<PlayerMovement>().killed();
         }
     }
 
     private void OnCollisionEnter2D(Collision2D other)
     {
-        if (other.gameObject.tag == "Player")
+        if (other.gameObject.name == "player(Clone)")
         {
             collidePlayer = true;
+            otherPlayer = other.gameObject;
+            Debug.Log(otherPlayer);
         }
     }
 
     private void OnCollisionExit2D(Collision2D other)
     {
-        collidePlayer = true;
+        collidePlayer = false;
+        // otherPlayer = null;
     }
 
     void FixedUpdate()
     {
         // Move the player character
         controller.Move(horizontalMove, verticalMove);
+    }
+
+    public void killed()
+    {
+        Debug.Log("I'm dead" + this.name);
+        animator.SetBool("Dead", true);
     }
 }
