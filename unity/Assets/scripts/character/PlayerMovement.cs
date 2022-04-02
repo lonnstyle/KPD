ea7ad79816e920a8ac138a37d2cab4f7db6375b7
lonnstyle <lonnstyle@gmail.com>
2022-04-02 15:16:53 +0800

    feat: collision detect TODO: broadcast message(kill/objective interact)

--- unity/Assets/scripts/character/PlayerMovement.cs
+++ unity/Assets/scripts/character/PlayerMovement.cs
@@ -9,12 +9,19 @@ public class PlayerMovement : MonoBehaviourPun
     public CharacterController2D controller;
     float horizontalMove = 0f;
     float verticalMove = 0f;
+    private Rigidbody2D m_Rigidbody2D;
 
+    private bool collidePlayer = false;
 
     // Start is called before the first frame update
     void Start()
     {
 
+    }
+
+    private void Awake()
+    {
+        m_Rigidbody2D = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -26,7 +33,23 @@ public class PlayerMovement : MonoBehaviourPun
         }//check if it is self character
         horizontalMove = Input.GetAxisRaw("Horizontal");
         verticalMove = Input.GetAxisRaw("Vertical");
-        
+        if (collidePlayer && Input.GetKeyDown(KeyCode.F))
+        {
+            Debug.Log("I'm trying to kill someone");
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            collidePlayer = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        collidePlayer = true;
     }
 
     void FixedUpdate()
