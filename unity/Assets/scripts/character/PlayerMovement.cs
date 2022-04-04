109f3b29b94b5a17d485710d07304352ecd03d7c
lonnstyle <54892447+lonnstyle@users.noreply.github.com>
2022-04-04 13:57:52 +0800

    Interact objects (#36)
    * feat: replace map and barrier
    
    * fix: remove unused controller code
    
    * feat: collision detect
    TODO: broadcast message(kill/objective interact)
    
    * feat: kill players

--- unity/Assets/scripts/character/PlayerMovement.cs
+++ unity/Assets/scripts/character/PlayerMovement.cs
@@ -9,12 +9,22 @@ public class PlayerMovement : MonoBehaviourPun
     public CharacterController2D controller;
     float horizontalMove = 0f;
     float verticalMove = 0f;
+    private Rigidbody2D m_Rigidbody2D;
 
+    private bool collidePlayer = false;
+    public GameObject otherPlayer;
+    private Animator animator;
 
     // Start is called before the first frame update
     void Start()
     {
 
+    }
+
+    private void Awake()
+    {
+        m_Rigidbody2D = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -26,11 +36,39 @@ public class PlayerMovement : MonoBehaviourPun
         }//check if it is self character
         horizontalMove = Input.GetAxisRaw("Horizontal");
         verticalMove = Input.GetAxisRaw("Vertical");
+        if (collidePlayer && Input.GetKeyDown(KeyCode.F))
+        {
+            Debug.Log("I'm trying to kill someone" + otherPlayer);
+            // otherPlayer.SendMessage("killed");
+            otherPlayer.GetComponent<PlayerMovement>().killed();
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.name == "player(Clone)")
+        {
+            collidePlayer = true;
+            otherPlayer = other.gameObject;
+            Debug.Log(otherPlayer);
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        collidePlayer = false;
+        // otherPlayer = null;
     }
 
     void FixedUpdate()
     {
         // Move the player character
         controller.Move(horizontalMove, verticalMove);
+    }
+
+    public void killed()
+    {
+        Debug.Log("I'm dead" + this.name);
+        animator.SetBool("Dead", true);
     }
 }
