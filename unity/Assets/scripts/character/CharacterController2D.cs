04b5b56211e403fac0970844aa99f5bf5234a913
lonnstyle <54892447+lonnstyle@users.noreply.github.com>
2022-04-04 13:57:52 +0800

    Interact objects (#36)
    * feat: replace map and barrier
    
    * fix: remove unused controller code
    
    * feat: collision detect
    TODO: broadcast message(kill/objective interact)
    
    * feat: kill players

--- unity/Assets/scripts/character/CharacterController2D.cs
+++ unity/Assets/scripts/character/CharacterController2D.cs
@@ -3,15 +3,7 @@ using UnityEngine.Events;
 
 public class CharacterController2D : MonoBehaviour
 {
-    [Range(0, .3f)] [SerializeField] private float m_MovementSmoothing = .05f;  // How much to smooth out the movement
-    [SerializeField] private LayerMask m_WhatIsGround;                          // A mask determining what is ground to the character
-                                                                            
-    [SerializeField] private Transform m_CeilingCheck;                          // A position marking where to check for ceilings
-    [SerializeField] private Collider2D m_CrouchDisableCollider;                // A collider that will be disabled when crouching
-
-    const float k_GroundedRadius = .2f; // Radius of the overlap circle to determine if grounded
-                                        // private bool m_Grounded;            // Whether or not the player is grounded.
-    const float k_CeilingRadius = .2f; // Radius of the overlap circle to determine if the player can stand up
+    [Range(0, .3f)][SerializeField] private float m_MovementSmoothing = .05f;  // How much to smooth out the movement
     private Rigidbody2D m_Rigidbody2D;
     private bool m_FacingRight = true;  // For determining which way the player is currently facing.
     private Vector3 m_Velocity = Vector3.zero;
@@ -21,20 +13,11 @@ public class CharacterController2D : MonoBehaviour
 
     public UnityEvent OnLandEvent;
 
-    [System.Serializable]
-    public class BoolEvent : UnityEvent<bool> { }
 
-    public BoolEvent OnCrouchEvent;
 
     private void Awake()
     {
         m_Rigidbody2D = GetComponent<Rigidbody2D>();
-
-        if (OnLandEvent == null)
-            OnLandEvent = new UnityEvent();
-
-        if (OnCrouchEvent == null)
-            OnCrouchEvent = new BoolEvent();
     }
 
     private void FixedUpdate()
